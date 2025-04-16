220bcdc29f4fb522b0955054339c68fc0fef25ec
Sirhurryup <dotcolumbia@gmail.com>
2025-04-16 16:29:47 -0400

    Update main_script.R

--- main_script.R
+++ main_script.R
@@ -6,8 +6,14 @@ print("Hello, Graph Classmates")
 lakers
 names(lakers)
 
-lakers %>% 
+jumpers_sas <- lakers %>% 
   filter(team == "SAS" & type == "jump") %>% 
   group_by(player, type) %>% 
-  summarise(total_points = max(points, na.rm = TRUE))
+  summarise(total_points = max(points, na.rm = TRUE)) %>% 
+  ungroup()
+
+write_csv(jumpers_sas, here("jumpers_sas.csv"))
+
+
+print("Hello, My R Peeps")
 
